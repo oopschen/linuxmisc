684160ea62a6d58299d8518af09f83357f513d01
Ray <linxray@gmail.com>
2020-01-10 17:44:40 +0800

    bugfix: no connected hdmi work

--- bash/monitor-op.sh
+++ bash/monitor-op.sh
@@ -6,7 +6,7 @@ if [ 1 -ne  $# ];then
 fi
 
 function query_monitor_name() {
-  echo $(xrandr -q | grep -E '\bconnected\b' | grep -i $1 | cut -d ' ' -f 1)
+  echo $(xrandr -q | grep -iE '\bconnected\b' | grep -i $1 | cut -d ' ' -f 1)
 }
 
 mainMonitor=$(query_monitor_name edp)
@@ -14,13 +14,27 @@ externalMonitor=$(query_monitor_name hdmi)
 
 case "$1" in
   hdmi-off)
-    xrandr --output $externalMonitor --off
+    for m in $externalMonitor
+    do
+      echo -e "turn off $m"
+      xrandr --output $m --off
+    done
     ;;
-  hdmi-on)
-    xrandr --output $externalMonitor --auto --primary --above $mainMonitor
-    ;;
-  hdmi-mirror)
-    xrandr --output $externalMonitor --auto --primary --same-as $mainMonitor
+  hdmi-on|hdmi-mirror)
+    if [ -z "$externalMonitor" ]; then
+      echo -e "no connected hdmi detected"
+      exit 1
+    fi
+
+    case "$1" in
+      hdmi-on)
+        xrandr --output $externalMonitor --auto --primary --above $mainMonitor
+        ;;
+      hdmi-mirror)
+        xrandr --output $externalMonitor --auto --primary --same-as $mainMonitor
+        ;;
+    esac
+
     ;;
   *)
     echo -e "Usage:\t ./$(basename $0) [hdmi-on|hdmi-off|hdmi-mirror]"
