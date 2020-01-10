075f55b27ffa5c625cba98d4868e396df0db9aa4
Ray <linxray@gmail.com>
2020-01-10 10:10:01 +0800

    feat: add config & update config

--- bash/start_win10.sh
+++ bash/start_win10.sh
@@ -4,7 +4,7 @@ diskdir=$(realpath ~/win10/disk-c)
 qemu-system-x86_64 --enable-kvm --name win10 -accel kvm -m 6G  \
   -drive file=${diskdir},format=raw,index=1,media=disk \
   -nic user,ipv6=off,smb=${sharedir}   -boot d -smp 4,cores=2,threads=2 -cpu host \
-  -display sdl -vga std -device qemu-xhci $@
+  -display sdl -vga std -device qemu-xhci -usb $@
 
 
 # Vendor=03f0 ProdID=ae07
