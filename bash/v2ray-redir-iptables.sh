54ebfcc3ad1943f45fd30ea244de61f7d4456433
Ray <linxray@gmail.com>
2020-03-29 14:15:38 +0800

    feat: add v2ray redir iptables

--- bash/v2ray-redir-iptables.sh
+++ bash/v2ray-redir-iptables.sh
@@ -5,14 +5,34 @@ if [ "$#" -lt 1 ]; then
 	exit 1
 fi
 
+function delete_chain_by_num() {
+  for n in $($cmd_iptables -t $1 -L $2 --line-number | grep $3 | cut -d ' ' -f 1)
+  do
+    $cmd_iptables -t $1 -D $2 $n
+  done
+}
+
+function disable_redir() {
+	echo "chain $chainName: disable"
+  delete_chain_by_num nat OUTPUT $chainName
+  delete_chain_by_num mangle PREROUTING $chainName
+  delete_chain_by_num mangle OUTPUT ${chainName}_MASK
+}
+
+function enable_redir() {
+	echo "chain $chainName: enable"
+	$cmd_iptables -t nat -A OUTPUT -p tcp -j $chainName
+  $cmd_iptables -t mangle -A PREROUTING -j $chainName
+  $cmd_iptables -t mangle -A OUTPUT -j ${chainName}_MASK
+}
+
 cmd_iptables="sudo iptables"
 cmd_ip="sudo ip"
 basedir=$(dirname $0)
-socksserver=104.224.142.84
+socksserver=199.247.14.156
 localport=1280
-echo -e "sever=$socksserver,localport=$localport udp_mode=$(test -z \"${USE_UDP}\" && 0)"
-
-
+dnsIgnoreIP="114.114.114.114,115.115.115.115,223.6.6.6,223.5.5.5,127.0.0.1"
+echo -e "server=$socksserver,localport=$localport"
 
 chainName=V2RAY
 mode=$1
@@ -21,6 +41,8 @@ case $mode in
 	# create rules
 	echo "create chain $chainName"
 	$cmd_iptables -t nat -N $chainName
+	$cmd_iptables -t mangle -N $chainName
+	$cmd_iptables -t mangle -N ${chainName}_MASK
 
 	# Ignore your $chainName server's addresses
 	echo "chain $chainName: ignore socks server: $socksserver"
@@ -36,9 +58,12 @@ case $mode in
 	$cmd_iptables -t nat -A $chainName -d 192.168.0.0/16 -j RETURN
 	$cmd_iptables -t nat -A $chainName -d 224.0.0.0/4 -j RETURN
 	$cmd_iptables -t nat -A $chainName -d 240.0.0.0/4 -j RETURN
+  ## udp
+  $cmd_iptables -t mangle -A $chainName -p udp -d $dnsIgnoreIP --dport 53 -j ACCEPT
+  $cmd_iptables -t mangle -A ${chainName}_MASK -p udp -d $dnsIgnoreIP --dport 53 -j ACCEPT
 
   # ignore ip in files
-  ignoreFile=~/.configx/.shadowsocks-redir-ignore
+  ignoreFile=~/.config/.shadowsocks-redir-ignore
   echo "check ignore file: $ignoreFile"
   if [ -f "$ignoreFile" ]; then
     echo "ignoring ips:  $(wc -l $ignoreFile)"
@@ -48,47 +73,51 @@ case $mode in
     done
   fi
 
-	# redirect to local port
-	echo "chain $chainName: redirect all tcp"
-	$cmd_iptables -t nat -A $chainName -p tcp -j REDIRECT --to-ports $localport
-
 	# Apply the rules
 	echo "chain $chainName: apply rules"
+  ## tcp
+	$cmd_iptables -t nat -A $chainName -p tcp -j REDIRECT --to-ports $localport
+
+  ## udp
+  $cmd_ip route add local default dev lo table 100
+  $cmd_ip rule add fwmark 1 lookup 100
+
+  $cmd_iptables -t mangle -A $chainName -p udp --dport 53 \
+    -j TPROXY --on-port $localport --tproxy-mark 0x01/0x01
+  $cmd_iptables -t mangle -A ${chainName}_MASK -p udp --dport 53 -j MARK --set-mark 1
+
 	# for desktop
-	$cmd_iptables -t nat -A OUTPUT -p tcp -j $chainName
+  enable_redir
 	echo "Done......"
 
 	;;
 
 	D)
-	echo "chain $chainName: flush chains"
-	$cmd_iptables -t nat -F $chainName
+  disable_redir
 
-	echo "chain $chainName: delete chains"
-	$cmd_iptables -t nat -D OUTPUT -p tcp -j $chainName
-
-	$cmd_iptables -t nat -X $chainName
+  $cmd_ip route del local default dev lo table 100
+  $cmd_ip rule del fwmark 1 lookup 100
 	echo "Done......"
 
 	;;
 
 	T)
-		inText=$($cmd_iptables -t nat -L OUTPUT -n| grep $chainName)
+		inText=$($cmd_iptables -t nat -L OUTPUT| grep $chainName)
 		msg=
 		if [ -z "$inText" ]; then
-			$cmd_iptables -t nat -A OUTPUT -p tcp -j $chainName
+      enable_redir
 			msg=UP
 		else
-			$cmd_iptables -t nat -D OUTPUT -p tcp -j $chainName
+      disable_redir
 			msg=DOWN
 		fi
-		echo "$msg Done......"
+		echo "$msg Down......"
 	;;
 
 	S)
-		inText=$($cmd_iptables -t nat -L OUTPUT -n| grep $chainName)
+		inText=$($cmd_iptables -t nat -L OUTPUT| grep $chainName)
 		if [ -z "$inText" ]; then
-			echo "Status Done..."
+			echo "Status Down..."
 		else
 			echo "Status UP..."
 		fi
