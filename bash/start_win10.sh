9df57b3f8fa1138c7e046130707610ccdd814ff4
Ray <linxray@gmail.com>
2020-07-07 08:43:49 +0800

    feat: use qemu 5

--- bash/start_win10.sh
+++ bash/start_win10.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sharedir=$(realpath ~/share)
 diskdir=$(realpath ~/vm/win10-c.qcow2)
-qemu-system-x86_64 --enable-kvm --name win10 -accel kvm -m 6G \
+qemu-system-x86_64 --enable-kvm --name win10 -machine accel=kvm -m 6G \
   -drive file=${diskdir},format=qcow2,index=1,media=disk \
   -nic user,ipv6=off,smb=${sharedir}   -boot d -smp 4,cores=2,threads=2 -cpu host \
   -display sdl -vga std -device qemu-xhci -usb $@
