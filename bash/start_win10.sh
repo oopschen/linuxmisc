3a38c85426ab5a27cf7ec680752cee9ef8a86f99
Ray <linxray@gmail.com>
2021-01-20 00:30:30 +0800

    feat: update vm win10 & i3

--- bash/start_win10.sh
+++ bash/start_win10.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 sharedir=$(realpath ~/share)
 diskdir=$(realpath ~/vm/win10-c.qcow2)
-qemu-system-x86_64 --enable-kvm --name win10 -machine accel=kvm -m 6G \
+
+cpufeaturesintel="pcid spec-ctrl stibp ssbd pdpe1gb md-clear mds-no taa-no tsx-ctrl"
+
+existedcpufeatures=""
+for f in $cpufeaturesintel
+do
+  finfo=$(grep "$f" /proc/cpuinfo)
+  if [ ! -z "$finfo" ]; then
+    existedcpufeatures="$existedcpufeatures,+$f"
+  fi
+done
+
+cpuinfo="Skylake-Client-noTSX-IBRS"
+
+qemu-system-x86_64 --enable-kvm --name win10 -m 6G -machine pc,accel=kvm \
   -drive file=${diskdir},format=qcow2,index=1,media=disk \
-  -nic user,ipv6=off,smb=${sharedir}   -boot d -smp 4,cores=2,threads=2 -cpu host \
-  -display sdl -vga std -device qemu-xhci -usb $@
+  -nic user,ipv6=off,smb=${sharedir}  -boot c \
+  -display sdl -vga std -device qemu-xhci \
+  -device usb-host,vendorid=0x05ac,productid=0x12a8 \
+  -device usb-host,vendorid=0x1ff7,productid=0x0200 \
+  -cpu $cpuinfo$existedcpufeatures -smp sockets=1,cores=3,threads=2 \
+  -object memory-backend-ram,size=6G,id=m0 \
+  -numa node,nodeid=0,memdev=m0 -numa cpu,node-id=0 \
+  $@
 
-
-# Vendor=03f0 ProdID=ae07
-# device_add driver=usb-host,vendorid=,productid=
+# iphone 8
+# wzga tou ping
